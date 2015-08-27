6e086fec82d63a51196e14d5bf19a756b1e0f15e
Alexey Kuznecov <lexa1thgmail.com>
2015-08-28 03:41:55 +0800

    File was changed

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,45 +1,46 @@
-$(function() {
+$(function () {
 
-	// Изначально скрываю все изображения и индексирую 
-	$('#slider img').each(function(i) {
-		$(this).attr('index', i).hide();
+    // Изначально скрываю все изображения и индексирую 
+    $('#slider img').each(function (i) {
+        $(this).attr('index', i).hide();
 
-	});
+    });
 
-	// Но одно оставляю как старотовая картинка
-	var firstImg = $("#slider img:first");
-	var lastImg = $("#slider img:last");
-	firstImg.show();
-	
-	// Генерация ссылок
-	$('#slider').append("<a href='#' id='arrowR' class='btn' name='arrowR'>>></a>");
-	$('#slider').prepend("<a href='#' id='arrowL' class='btn' name='arrowR'><<</a>");
+    // Но одно оставляю как старотовая картинка
+    var firstImg = $("#slider img:first");
+    var lastImg = $("#slider img:last");
+    firstImg.show();
 
-	// Функция для перехода к следующиему изображению
-	function nextImg() {		
-		$('#slider img:visible').attr('id', 'currentImg');
-		$('#slider img:hidden').removeAttr('id');
-		$('#currentImg').fadeOut(1000).next().fadeIn(1000);
-		console.log($('#currentImg').attr('index'));
-	}
+    // Генерация ссылок
+    $('#slider').append("<a href='#' id='arrowR' class='btn' name='arrowR'>>></a>");
+    $('#slider').prepend("<a href='#' id='arrowL' class='btn' name='arrowR'><<</a>");
 
-	function prevImg() {
-		$('#slider img:visible').hide().prev().show();
-		$(this).hide();
-	}
-	
-	// События мышы 
-        $('#arrowR').click(function(e) {
-		e.preventDefault();	
-		nextImg();		
-	});
+    // Функция для перехода к следующиему изображению
+    function nextImg() {
+        $('#slider img:visible').attr('id', 'currentImg');
+        $('#slider img:hidden').removeAttr('id');
+        $('#currentImg').fadeOut(1000).next().fadeIn(1000);
+        console.log($('#currentImg').attr('index'));
+    }
 
-	$('#arrowL').click(function(e) {
-		e.preventDefault();
-		prevImg();
-		if ($('#slider img:visible').attr('src') === undefined) lastImg.show();
-		console.log($('#slider img:visible').attr('src'));
-	});
+    function prevImg() {
+        $('#slider img:visible').hide().prev().show();
+        $(this).hide();
+    }
+
+    // События мышы 
+    $('#arrowR').click(function (e) {
+        e.preventDefault();
+        nextImg();
+    });
+
+    $('#arrowL').click(function (e) {
+        e.preventDefault();
+        prevImg();
+        if ($('#slider img:visible').attr('src') === undefined)
+            lastImg.show();
+        console.log($('#slider img:visible').attr('src'));
+    });
 
 });// End
 
